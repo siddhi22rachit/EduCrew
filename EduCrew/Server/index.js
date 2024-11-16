3e8ae47155c9e4743d989322bbd29f94e63a31a9
TulsiCodeCraft <152911625+TulsiCodeCraft@users.noreply.github.com>
2024-11-16 18:10:50 +0530

    some changes

--- EduCrew/Server/index.js
+++ EduCrew/Server/index.js
@@ -1,22 +1,18 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-dotenv.config();
+ import mongoose from 'mongoose'; 
+ import dotenv from 'dotenv'; 
+ import userRoutes from './routes/user.route.js';
+  import authRoutes from './routes/auth.route.js';
+   import cors from 'cors';
+    import cookieParser from 'cookie-parser'; 
+    import path from 'path';
+     dotenv.config();
 
-
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(process.env.MONGO).then(() => { 
+  console.log('Connected to MongoDB');
+ }).catch((err) => { 
+  console.log(err); 
+});
 
 const __dirname = path.resolve();
 
@@ -26,27 +22,20 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html')); });
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(3000, () => { 
+  console.log('Server listening on port 3000'); 
 });
 
-app.use('/api/user', userRoutes);
+app.use('/api/user', userRoutes); 
 app.use('/api/auth', authRoutes);
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    message,
-    statusCode,
-  });
-});
+app.use((err, req, res, next) => { 
+  const statusCode = err.statusCode || 500; 
+  const message = err.message || 'Internal Server Error'; 
+  return res.status(statusCode).json({ success: false, message, statusCode, }); });
