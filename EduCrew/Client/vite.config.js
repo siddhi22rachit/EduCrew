a6f9fbd9da0da85a90f585e6e9c47fdd3aee8b24
siddhi22rachit <siddhibsawant2212@gmail.com>
2025-08-16 19:03:01 +0530

    Revert "changes in client"
    This reverts commit b328a777d5926b88a51a1517fb48ea0976dd4a99.

--- EduCrew/Client/vite.config.js
+++ EduCrew/Client/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: ' https://educrew-2.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
     },
     
